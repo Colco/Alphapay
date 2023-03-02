61a503395933844a4787abaf1b7295891c08aae9
chen hao <hao.chen@mail.dealmoon.com>
2023-03-02 16:09:44 +0800

    fix: home page data log

--- src/views/data-log/index.vue
+++ src/views/data-log/index.vue
@@ -4,11 +4,14 @@ import { ElMessage } from 'element-plus'
 import { findTradeSituation } from '~/api/api';
 import { useRouter, useRoute } from 'vue-router'
 import { useStore } from '~/store/store';
+import { fa } from "element-plus/es/locale";
 
 const store = useStore()
 
 const router = useRouter();
 const route = useRoute();
+
+const loading = ref(true);
 
 interface  DataLog {
   totalTradeAmount: number
@@ -40,9 +43,10 @@ interface  DataLog {
   yesterdaySuccessRate: number
 }
 
-let sitlog = reactive(<DataLog>{});
+let sitlog = reactive({data: <DataLog>{}});
 findTradeSituation().then(res => {
-    sitlog = res.data.data;
+    sitlog.data = res.data.data;
+    loading.value = false;
 })
 
 </script>
@@ -50,7 +54,7 @@ findTradeSituation().then(res => {
 <template>
   <div class="charts-box">
     <div class="card-box">
-    <el-card class="box-card">
+    <el-card v-loading="loading" class="box-card">
         <template #header>
         <div class="card-header">
             <span>{{$t('datalog.title1')}}</span>
@@ -58,19 +62,19 @@ findTradeSituation().then(res => {
         </div>
         </template>
         <div>
-            <h1>{{sitlog.totalTradeAmount}}VAD</h1>
+            <h1>{{sitlog.data.totalTradeAmount}}VAD</h1>
         </div>
         <div class="tips">
-            <p>{{$t('datalog.service_charge')}}: <span>{{sitlog.todayPoundage}}VAD</span></p>
-            <p>{{$t('datalog.paid_in_amount')}}: <span>{{sitlog.todayActualIncome}}VAD</span></p>
+            <p>{{$t('datalog.service_charge')}}: <span>{{sitlog.data.totalPoundage}}VAD</span></p>
+            <p>{{$t('datalog.paid_in_amount')}}: <span>{{sitlog.data.totalActualIncome}}VAD</span></p>
         </div>
         <div class="tips">
-            <p>{{$t('datalog.order_quantity_paid')}}: <span>{{sitlog.totalPaidOrderNum}}</span></p>
-            <p>{{$t('datalog.order_quantity')}}: <span>{{sitlog.todayOrderNum}}</span></p>
-            <p>{{$t('datalog.success_rate')}}: <span>{{sitlog.todaySuccessRate}}%</span></p>
+            <p>{{$t('datalog.order_quantity_paid')}}: <span>{{sitlog.data.totalPaidOrderNum}}</span></p>
+            <p>{{$t('datalog.order_quantity')}}: <span>{{sitlog.data.totalOrderNum}}</span></p>
+            <p>{{$t('datalog.success_rate')}}: <span>{{sitlog.data.totalSuccessRate}}%</span></p>
         </div>
   </el-card>
-  <el-card class="box-card">
+  <el-card v-loading="loading" class="box-card">
         <template #header>
         <div class="card-header">
             <span>{{$t('datalog.title3')}}</span>
@@ -78,19 +82,19 @@ findTradeSituation().then(res => {
         </div>
         </template>
         <div>
-            <h1>{{sitlog.monthTradeAmount}}VAD</h1>
+            <h1>{{sitlog.data.monthTradeAmount}}VAD</h1>
         </div>
         <div class="tips">
-            <p>{{$t('datalog.service_charge')}}: <span>{{sitlog.monthPoundage}}VAD</span></p>
-            <p>{{$t('datalog.paid_in_amount')}}: <span>{{sitlog.monthActualIncome}}VAD</span></p>
+            <p>{{$t('datalog.service_charge')}}: <span>{{sitlog.data.monthPoundage}}VAD</span></p>
+            <p>{{$t('datalog.paid_in_amount')}}: <span>{{sitlog.data.monthActualIncome}}VAD</span></p>
         </div>
         <div class="tips">
-            <p>{{$t('datalog.order_quantity_paid')}}: <span>{{sitlog.monthPaidOrderNum}}</span></p>
-            <p>{{$t('datalog.order_quantity')}}: <span>{{sitlog.monthOrderNum}}</span></p>
-            <p>{{$t('datalog.success_rate')}}: <span>{{sitlog.monthSuccessRate}}%</span></p>
+            <p>{{$t('datalog.order_quantity_paid')}}: <span>{{sitlog.data.monthPaidOrderNum}}</span></p>
+            <p>{{$t('datalog.order_quantity')}}: <span>{{sitlog.data.monthOrderNum}}</span></p>
+            <p>{{$t('datalog.success_rate')}}: <span>{{sitlog.data.monthSuccessRate}}%</span></p>
         </div>
   </el-card>
-  <el-card class="box-card">
+  <el-card v-loading="loading" class="box-card">
         <template #header>
         <div class="card-header">
             <span>{{$t('datalog.title2')}}</span>
@@ -98,19 +102,19 @@ findTradeSituation().then(res => {
         </div>
         </template>
         <div>
-            <h1>{{sitlog.yesterdayTradeAmount}}VAD</h1>
+            <h1>{{sitlog.data.yesterdayTradeAmount}}VAD</h1>
         </div>
         <div class="tips">
-            <p>{{$t('datalog.service_charge')}}: <span>{{sitlog.yesterdayPoundage}}VAD</span></p>
-            <p>{{$t('datalog.paid_in_amount')}}: <span>{{sitlog.yesterdayActualIncome}}VAD</span></p>
+            <p>{{$t('datalog.service_charge')}}: <span>{{sitlog.data.yesterdayPoundage}}VAD</span></p>
+            <p>{{$t('datalog.paid_in_amount')}}: <span>{{sitlog.data.yesterdayActualIncome}}VAD</span></p>
         </div>
         <div class="tips">
-            <p>{{$t('datalog.order_quantity_paid')}}: <span>{{sitlog.yesterdayPaidOrderNum}}</span></p>
-            <p>{{$t('datalog.order_quantity')}}: <span>{{sitlog.yesterdayOrderNum}}</span></p>
-            <p>{{$t('datalog.success_rate')}}: <span>{{sitlog.yesterdaySuccessRate}}%</span></p>
+            <p>{{$t('datalog.order_quantity_paid')}}: <span>{{sitlog.data.yesterdayPaidOrderNum}}</span></p>
+            <p>{{$t('datalog.order_quantity')}}: <span>{{sitlog.data.yesterdayOrderNum}}</span></p>
+            <p>{{$t('datalog.success_rate')}}: <span>{{sitlog.data.yesterdaySuccessRate}}%</span></p>
         </div>
   </el-card>
-  <el-card class="box-card">
+  <el-card v-loading="loading" class="box-card">
         <template #header>
         <div class="card-header">
             <span>{{$t('datalog.title4')}}</span>
@@ -118,16 +122,16 @@ findTradeSituation().then(res => {
         </div>
         </template>
         <div>
-            <h1>{{sitlog.todayTradeAmount}}VAD</h1>
+            <h1>{{sitlog.data.todayTradeAmount}}VAD</h1>
         </div>
         <div class="tips">
-            <p>{{$t('datalog.service_charge')}}: <span>{{sitlog.todayPoundage}}VAD</span></p>
-            <p>{{$t('datalog.paid_in_amount')}}: <span>{{sitlog.todayActualIncome}}VAD</span></p>
+            <p>{{$t('datalog.service_charge')}}: <span>{{sitlog.data.todayPoundage}}VAD</span></p>
+            <p>{{$t('datalog.paid_in_amount')}}: <span>{{sitlog.data.todayActualIncome}}VAD</span></p>
         </div>
         <div class="tips">
-            <p>{{$t('datalog.order_quantity_paid')}}: <span>{{sitlog.todayPaidOrderNum}}</span></p>
-            <p>{{$t('datalog.order_quantity')}}: <span>{{sitlog.todayOrderNum}}</span></p>
-            <p>{{$t('datalog.success_rate')}}: <span>{{sitlog.todaySuccessRate}}%</span></p>
+            <p>{{$t('datalog.order_quantity_paid')}}: <span>{{sitlog.data.todayPaidOrderNum}}</span></p>
+            <p>{{$t('datalog.order_quantity')}}: <span>{{sitlog.data.todayOrderNum}}</span></p>
+            <p>{{$t('datalog.success_rate')}}: <span>{{sitlog.data.todaySuccessRate}}%</span></p>
         </div>
   </el-card>
     </div>
